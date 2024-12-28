60058f8db7dfb02e9889877b89628a0aac26271b
inaemin <inaemin@gmail.com>
2024-12-29 06:02:11 +0900

    feat: 배포설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/vue-todo/", // 이렇게 설정
   plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
